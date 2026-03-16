ea63ac7288034df6ee4d791f20c0b90153e9eacb
canopus-notebooks <omarbory@gmail.com>
2026-03-16 14:58:52 +0200

    Update u3c2s5.js

--- data/u3c2s5.js
+++ data/u3c2s5.js
@@ -1,26 +1,21 @@
 const lessonData = {
   title: "Pollution, Fossil Fuels, and Energy Conservation",
   info: "Junior 4 Science • Unit 3 • Concept 2 • Session 5",
-
   slides: [
-
     {
       title: "Introduction",
       text: "In this session we will learn about pollution and fossil fuels.",
       image: "assets/images/u3c2intro.jpg"
     },
-
     {
       title: "Activity 9",
       text: "Big cities suffer from environmental problems.",
       image: "assets/images/u3c2a9.jpg"
     },
-
     {
       title: "Conclusion",
       text: "Conserving fossil fuels helps protect the environment.",
       image: "assets/images/u3c2summary.jpg"
     }
-
   ]
 };
