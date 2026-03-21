1519d5796089f2ec5787a106f4f3ed03c0c47233
canopus-notebooks <omarbory@gmail.com>
2026-03-21 13:12:51 +0200

    Create u3c1s1.js

---
+++ data/u3c1s1.js
@@ -0,0 +1,181 @@
+const imgPath = "assets/images/s1/";
+
+const lessonData = {
+  title: "Energy and Energy Transformation",
+  info: "Junior 4 Science - Unit 3 - Concept 1 - Session 1",
+
+  slides: [
+    {
+      title: "Activity 1",
+      text: "Energy can be converted from one form to another.",
+
+      cards: [
+        {
+          image: imgPath + "u3c1a1.jpg",
+          title: "The Sun",
+          text: "Most of the energy we use comes from the <span class='highlight'>Sun</span>."
+        },
+        {
+          image: imgPath + "u3c1a12.jpg",
+          title: "Solar Cells",
+          text: "Solar cells convert <span class='highlight'>solar energy</span> into <span class='highlight'>electrical energy</span>."
+        }
+      ],
+
+      sections: [
+        {
+          heading: "Important Facts",
+          bullets: [
+            "Most of the energy we use comes from the <span class='highlight'>Sun</span>",
+            "Energy can be changed from one form to another",
+            "Devices need <span class='highlight'>electrical energy</span> to operate"
+          ]
+        },
+        {
+          heading: "Energy Conversion",
+          bullets: [
+            "<span class='keyword'>Solar energy → Electrical energy</span>"
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Activity 2",
+      text: "Energy in remote-controlled cars.",
+
+      cards: [
+        {
+          image: imgPath + "u3c1a2.jpg",
+          title: "Remote-Controlled Car",
+          text: "Toys need <span class='highlight'>energy</span> to move and work."
+        },
+        {
+          image: imgPath + "u3c1a22.jpg",
+          title: "Batteries",
+          text: "Batteries are the source of energy and store <span class='highlight'>chemical energy</span>."
+        }
+      ],
+
+      sections: [
+        {
+          heading: "Energy Changes",
+          bullets: [
+            "<span class='highlight'>Chemical energy</span> is converted into <span class='highlight'>electrical energy</span>",
+            "<span class='highlight'>Electrical energy</span> is converted into <span class='highlight'>kinetic energy</span> and <span class='highlight'>sound energy</span>"
+          ]
+        },
+        {
+          heading: "When Batteries Run Out",
+          bullets: [
+            "Recharge batteries",
+            "Replace batteries"
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Activity 3",
+      text: "Mars rover needs energy to operate.",
+
+      cards: [
+        {
+          image: imgPath + "u3c1a3.jpg",
+          title: "Mars Rover",
+          text: "It uses <span class='highlight'>solar panels</span> and <span class='highlight'>batteries</span>."
+        }
+      ],
+
+      sections: [
+        {
+          heading: "How It Works",
+          bullets: [
+            "Solar panels convert <span class='highlight'>solar energy</span> into <span class='highlight'>electrical energy</span>",
+            "Electrical energy is used to <span class='highlight'>charge batteries</span>",
+            "Electrical energy is converted into <span class='highlight'>kinetic energy</span> and <span class='highlight'>thermal energy</span>"
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Activity 4",
+      text: "Energy changes from one form to another.",
+
+      cards: [
+        {
+          image: imgPath + "u3c1a4.jpg",
+          title: "Hair Dryer",
+          text: "<span class='keyword'>Electrical energy → Thermal energy + Kinetic energy + Sound energy</span>"
+        },
+        {
+          image: imgPath + "u3c1a42.jpg",
+          title: "Washing Machine",
+          text: "<span class='keyword'>Electrical energy → Kinetic energy + Sound energy</span>"
+        }
+      ],
+
+      sections: [
+        {
+          heading: "Examples of Energy Transformation",
+          bullets: [
+            "Hair dryer: Electrical energy → Thermal energy + Kinetic energy + Sound energy",
+            "Soap dispenser: Potential energy → Kinetic energy",
+            "Washing machine: Electrical energy → Kinetic energy + Sound energy"
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Activity 5",
+      text: "Energy chains describe the flow of energy and its conversion.",
+
+      cards: [
+        {
+          image: imgPath + "u3c1a5.jpg",
+          title: "Energy Chain",
+          text: "Energy chains usually start from the <span class='highlight'>Sun</span>."
+        }
+      ],
+
+      sections: [
+        {
+          heading: "Examples",
+          bullets: [
+            "Food: <span class='keyword'>Light energy → Chemical energy → Kinetic energy</span>",
+            "Heating water: <span class='keyword'>Light energy → Chemical energy → Thermal energy</span>",
+            "Hair dryer: <span class='keyword'>Light energy → Chemical energy → Electrical energy → Thermal + Kinetic + Sound</span>"
+          ]
+        },
+        {
+          heading: "Notes",
+          bullets: [
+            "Not all energy reaches the device",
+            "Some energy is <span class='highlight'>wasted</span>",
+            "Wasted energy is usually in the form of <span class='highlight'>heat</span>"
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Summary",
+      image: imgPath + "u3c1summary.jpg",
+
+      bullets: [
+        "Most energy comes from the <span class='highlight'>Sun</span>",
+        "Energy can be converted from one form to another",
+        "Batteries store <span class='highlight'>chemical energy</span>",
+        "<span class='keyword'>Chemical energy → Electrical energy</span>",
+        "<span class='keyword'>Electrical energy → Kinetic energy + Sound energy</span>",
+        "Solar energy can be converted into <span class='highlight'>electrical energy</span>",
+        "Energy chains show how energy moves",
+        "Energy is not destroyed but converted",
+        "Some energy is wasted as <span class='highlight'>heat</span>",
+        "Devices need energy to work"
+      ]
+    }
+  ]
+};
