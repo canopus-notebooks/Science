7bcc9ceab73cc9f3c65787084335794452d596f3
canopus-notebooks <omarbory@gmail.com>
2026-03-21 11:47:17 +0200

    Update u3c2s5.js

--- data/u3c2s5.js
+++ data/u3c2s5.js
@@ -3,126 +3,151 @@ const lessonData = {
   info: "Junior 4 Science - Unit 3 - Concept 2 - Session 5",
 
   slides: [
+
     {
       title: "Activity 9",
-      text: "Big cities have many environmental problems because of human activities.",
+      text: "Big cities have many <span class='highlight'>environmental problems</span> because of <span class='highlight'>human activities</span>.",
+
       cards: [
         {
           image: "u3c2a9.jpg",
           title: "Factories",
-          text: "Factories produce smoke."
+          text: "Factories produce <span class='highlight'>smoke</span>."
         },
         {
           image: "u3c2a9.jpg",
           title: "Cars",
-          text: "Cars produce smog."
+          text: "Cars produce <span class='highlight'>smog</span>."
         },
         {
           image: "u3c2a9.jpg",
           title: "Farms",
-          text: "Farms use pesticides."
+          text: "Farms use <span class='highlight'>pesticides</span>."
         }
       ],
+
       bullets: [
-        "All these cause pollution",
-        "Types of pollution: air pollution, water pollution, and soil pollution",
-        "Smog causes irritation of eyes and lungs",
-        "Small particles damage the respiratory system"
+        "Burning of fuels produces <span class='highlight'>smog</span> that pollutes the air",
+        "<span class='highlight'>Pesticides</span> mix with water and pollute soil and rivers",
+        "<span class='highlight'>Chemicals</span> from factories pollute air, water, and soil",
+
+        "Types of pollution:",
+        "<span class='keyword'>Air</span> – <span class='keyword'>Water</span> – <span class='keyword'>Soil</span>",
+
+        "Smog causes irritation of <span class='highlight'>eyes</span> and <span class='highlight'>lungs</span>",
+        "Small particles damage the <span class='highlight'>respiratory system</span>"
       ]
     },
 
     {
       title: "Activity 10",
-      text: "Burning fossil fuels produces energy but causes pollution.",
+      text: "Burning <span class='highlight'>fossil fuels</span> produces energy but causes pollution.",
+
       cards: [
         {
           image: "u3c2a10.jpg",
           title: "Fossil Fuels",
-          text: "Examples: coal, natural gas, oil, and gasoline."
+          text: "<span class='highlight'>Coal</span>, <span class='highlight'>oil</span>, <span class='highlight'>natural gas</span>, gasoline."
         },
         {
           image: "u3c2a102.jpg",
           title: "Acid Rain",
-          text: "CO2 combines with water in the air and forms acid rain."
+          text: "<span class='highlight'>CO2</span> + water → <span class='highlight'>acid rain</span>"
         },
         {
           image: "u3c2a103.jpg",
           title: "Global Warming",
-          text: "CO2 forms a layer in the atmosphere and traps heat."
+          text: "<span class='highlight'>CO2</span> traps heat"
         }
       ],
+
       bullets: [
-        "When we burn fossil fuels, carbon dioxide (CO2) is formed",
-        "Acid rain causes death of trees and fish",
-        "Acid rain changes the chemical nature of soil and lakes",
-        "Acid rain dissolves rocks in some buildings",
-        "Global warming causes increase in Earth's temperature"
+        "Burning fossil fuels produces <span class='highlight'>carbon dioxide (CO2)</span>",
+
+        "<span class='keyword'>Acid rain:</span>",
+        "<span class='highlight'>CO2</span> combines with water to form acid rain",
+        "Causes <span class='highlight'>death of trees</span> and <span class='highlight'>fish</span>",
+        "Changes the <span class='highlight'>chemical nature</span> of soil and lakes",
+        "Dissolves <span class='highlight'>rocks</span>",
+
+        "<span class='keyword'>Global warming:</span>",
+        "<span class='highlight'>CO2</span> forms a layer in the atmosphere",
+        "This layer <span class='highlight'>traps heat</span>",
+        "Causes increase in <span class='highlight'>Earth's temperature</span>"
       ]
     },
 
     {
       title: "Activity 11",
-      text: "We should conserve fossil fuels because they take millions of years to form.",
+      text: "We should <span class='highlight'>conserve fossil fuels</span> because they take millions of years to form.",
+
       cards: [
         {
           image: "u3c2a11.jpg",
-          title: "Use Bicycle",
-          text: "Use bicycles instead of cars."
+          title: "Save Fuel",
+          text: "Use less <span class='highlight'>fuel</span>"
         },
         {
           image: "u3c2a112.jpg",
           title: "Turn Off Lights",
-          text: "Turn off lights when not needed."
+          text: "Save <span class='highlight'>electricity</span>"
         },
         {
           image: "u3c2a113.jpg",
           title: "Renewable Energy",
-          text: "Use renewable energy like solar and wind."
+          text: "<span class='highlight'>Solar</span> and <span class='highlight'>wind</span>"
         }
       ],
+
       bullets: [
-        "Fossil fuels are limited",
-        "Fossil fuels are nonrenewable",
-        "So, we must conserve them"
+        "Fossil fuels are <span class='highlight'>limited</span>",
+        "Fossil fuels are <span class='highlight'>nonrenewable</span>",
+
+        "<span class='keyword'>Ways to conserve energy:</span>",
+        "Using <span class='highlight'>bicycle</span> instead of cars",
+        "Turning off <span class='highlight'>lights</span>",
+        "Using <span class='highlight'>renewable energy</span>"
       ]
     },
 
     {
       title: "Activity 12",
-      text: "There are renewable and nonrenewable energy resources.",
+      text: "There are <span class='highlight'>renewable</span> and <span class='highlight'>nonrenewable</span> energy resources.",
+
       cards: [
         {
           image: "u3c2a12.jpg",
           title: "Renewable Energy",
-          text: "Solar, water, charcoal, wind, and wood."
+          text: "Can be <span class='highlight'>replaced</span>"
         },
         {
           image: "u3c2a122.jpg",
           title: "Nonrenewable Energy",
-          text: "Coal, gasoline, oil, and natural gas."
+          text: "Cannot be replaced quickly"
         }
       ],
+
       bullets: [
-        "Renewable energy resources can be replaced",
-        "Nonrenewable energy resources cannot be replaced quickly"
+        "<span class='keyword'>Renewable:</span> Solar – Water – Wind – Wood",
+        "<span class='keyword'>Nonrenewable:</span> Coal – Oil – Gas"
       ]
     },
 
     {
       title: "Summary",
       image: "u3c2summary.jpg",
+
       bullets: [
-        "Pollution is caused by burning fuels, chemicals, and pesticides",
-        "Air pollution is caused by smog from cars and factories",
-        "Water and soil pollution are caused by chemicals and pesticides",
-        "Smog irritates the eyes and lungs and damages the respiratory system",
-        "Fossil fuels include coal, oil, and natural gas",
-        "Fossil fuels are nonrenewable and limited resources",
-        "Burning fossil fuels produces carbon dioxide",
-        "Carbon dioxide causes acid rain and global warming",
-        "Acid rain harms trees, fish, soil, and buildings",
-        "Pollution can be reduced by saving energy and using renewable energy"
+        "<span class='highlight'>Pollution</span> is caused by human activities",
+        "<span class='highlight'>Smog</span> affects eyes and lungs",
+        "<span class='highlight'>Fossil fuels</span> are nonrenewable",
+        "Burning fuels produces <span class='highlight'>CO2</span>",
+        "<span class='highlight'>CO2</span> causes acid rain and global warming",
+        "<span class='highlight'>Acid rain</span> damages environment",
+        "<span class='highlight'>Global warming</span> increases temperature",
+        "We must use <span class='highlight'>renewable energy</span>"
       ]
     }
+
   ]
 };
